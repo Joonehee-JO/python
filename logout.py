1466d0c3aca4d47f0bba199c17e943f28571391e
Joonehee-JO <63311325+Joonehee-JO@users.noreply.github.com>
2020-06-23 13:59:14 +0900

    로그아웃 기능 코드
    로그아웃 기능 코드

---
+++ logout.py
@@ -0,0 +1,15 @@
+#작성자 : 조준희
+#학과 : 소프트웨어
+#학번 : 2017038076
+#조 : 파이썬클라쓰(4조)
+#프로그램 내용 : 홈페이지 회원가입과 로그인, 로그아웃, 아이디 찾기, 비밀번호 찾기
+#최종 완성일자 : 6월 14일
+###########################
+def logout(request) : #로그아웃을 위한 기능 / User모델 각 객체의 필드 중 상태필드를 활용하여 해당 고객이 로그인상태인지 확인하고 로그아웃을 처리한다.
+    user = User.objects.all()
+    for item in user :
+        if item.status == 1 :
+            item.status = 0
+            item.save()
+            login = 0
+            return render(request, 'main.html', {'login':login})
