3e93665b6d391e377a644ed4ccd27ab4d3593437
Joonehee-JO <63311325+Joonehee-JO@users.noreply.github.com>
2020-06-23 21:58:21 +0900

    2017038076_조준희 2주차 과제
    2017038076_조준희 2주차 과제

--- 2017038076_조준희 2주차과제.py
+++ 2017038076_조준희 2주차과제.py
@@ -2,4 +2,6 @@ mem = int(input("참석자의 수를 입력하세요 : "))
 chicken = mem * 1
 beer = mem * 2
 cake = mem * 4
-print("치킨의 수 : %d\n맥주의 수 : %d\n케잌의 수 : %d" %chicken %beer %cake)
+print("치킨의 수 : %d\n" %chicken)
+print("맥주의 수 : %d\n" %beer)
+print("케잌의 수 : %d\n" %cake)
